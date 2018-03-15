cbfb19c2df5c04e17a24a4f9d719d6ff83662113
Amal Shajan <amalshajan2011@gmail.com>
2018-03-15 11:43:01 +0530

    connect to existing wifi

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-const char *ssid     = "PloBot 2.0";
-const char *password = "helloworld";
+#define WIFI_SSID "plobot"
+#define WIFI_PASS "workinprogress"
 
 ESP8266WebServer server(80);
 
@@ -32,19 +32,25 @@ void handleText() {
   }
 }
 
+void setupWiFi(){
+    Serial.println("\nConnecting...");
+    WiFi.begin(WIFI_SSID,WIFI_PASS);
+    while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(500);
+      }
+     Serial.println("\nConnected : ");
+     Serial.println(WiFi.localIP());
+  }
+
 void setup() {
 	delay(1000);
 
 	Serial.begin(115200);
 	Serial.println();
-	Serial.print("Configuring access point...");
-	/* uncomment the passeord to make the AP closed */
-	//WiFi.softAP(ssid, password);
-	WiFi.softAP(ssid);
 
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
+	Serial.println("Setting Up WiFi..");
+	setupWiFi();
 
 	server.on("/", handleRoot);
 	server.on("/getText", handleText);
